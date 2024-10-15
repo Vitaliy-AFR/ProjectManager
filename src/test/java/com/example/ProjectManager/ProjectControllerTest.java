4a1f382b0168fd53c590aa04527e1a9ce9d6896a
Vitaliy-AFR <vitaliy.afr@yandex.ru>
2024-10-15 23:18:00 +0300

    Разработка теста для контроллера. Добавлен тест для метода findAllProjects

--- src/test/java/com/example/ProjectManager/ProjectControllerTest.java
+++ src/test/java/com/example/ProjectManager/ProjectControllerTest.java
@@ -1,21 +1,44 @@
 package com.example.ProjectManager;
 
 import com.example.ProjectManager.controller.ProjectController;
+import com.example.ProjectManager.service.ProjectService;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
-@SpringBootTest
+@WebMvcTest(ProjectController.class)
 public class ProjectControllerTest  {
 
+    @MockBean
+    private ProjectService projectService;
+
     @Autowired
-    private ProjectController controller;
+    private MockMvc mockMvc;
+    @Autowired
+    private ObjectMapper objectMapper;
+
+//    @Test
+//    public void loginTest() throws Exception {
+//        this.mockMvc.perform(get("/api/v1/projects"))
+//                .andExpect(status().is3xxRedirection());
+//    }
+
 
     @Test
-    public void test() throws Exception {
-        assertThat(controller).isNotNull();
+    @WithMockUser(roles = "ADMIN", password = "admin", username = "admin")
+    public void findAllProjectsTest() throws Exception {
+        this.mockMvc.perform(get("/api/v1/projects"))
+                .andDo(print())
+                .andExpect(status().isOk());
     }
+
+
 
 }
