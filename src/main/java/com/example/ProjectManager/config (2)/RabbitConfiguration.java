19afbb7d4a970b844e87fee3c3f9c30ffdc97b73
Vitaliy-AFR <vitaliy.afr@yandex.ru>
2024-12-13 19:35:41 +0300

    Добавлен Rabbit MQ

--- src/main/java/com/example/ProjectManager/config (2)/RabbitConfiguration.java
+++ src/main/java/com/example/ProjectManager/config (2)/RabbitConfiguration.java
@@ -2,6 +2,8 @@ package com.example.ProjectManager.config;
 
 import lombok.Setter;
 import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitAdmin;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,9 +15,31 @@ public class RabbitConfiguration {
     @Value("${queue.name}")
     private String queueName;
 
+    @Value("${spring.rabbitmq.username}")
+    private String username;
+
+    @Value("${spring.rabbitmq.password}")
+    private String password;
+
+
     @Bean
     public Queue queue() {
-        return new Queue(queueName, false);
+        Queue queue = new Queue(queueName, false);
+        rabbitAdmin().declareQueue(queue);
+        return queue;
+    }
+
+    @Bean
+    public CachingConnectionFactory connectionFactory() {
+        CachingConnectionFactory connectionFactory = new CachingConnectionFactory("localhost");
+        connectionFactory.setUsername(username);
+        connectionFactory.setPassword(password);
+        return connectionFactory;
+    }
+
+    @Bean
+    public RabbitAdmin rabbitAdmin(){
+        return new RabbitAdmin(connectionFactory());
     }
 
 }
